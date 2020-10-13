17e5fc08e4ff2224dd804d192db22d63887d1164
Julio Benavente <32945829+julio-benavente@users.noreply.github.com>
2020-10-13 12:30:32 -0500

    MongoDB connected

---
+++ configuration/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://julio:julio@exprensetracker.jlhzk.mongodb.net/fstodolist?retryWrites=true&w=majority",
+};
---
+++ models/items.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const ItemSchema = new Schema({
+  todo: {
+    type: String,
+    required: true,
+  },
+  checked: {
+    type: Boolean,
+    default: false,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = Item = mongoose.model("item", ItemSchema);
